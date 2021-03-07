b1b66abb6b8b3b625d3de18b1eeecc463699eb6d
Anika Bernstein <anikajb92@gmail.com>
2021-03-07 13:34:01 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// remove the node main#main
+document.querySelector('main#main').remove();
+
+//Create a new element here and assin it to a newHeader
+const newHeader = document.createElement("h1");
+newHeader.id = ('victory');
+newHeader.victory = document.body.appendChild(newHeader);
+newHeader.innerHTML = "<h1>Anika is the champion!</h1>";
